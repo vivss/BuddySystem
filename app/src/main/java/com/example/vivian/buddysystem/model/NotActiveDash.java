7b2fc43ebdaa88610705b985250127b56ad1d561
vivss <vli4@u.rochester.edu>
2018-02-10 22:52:51 -0500

    added not active dashboard

---
+++ app/src/main/java/com/example/vivian/buddysystem/model/NotActiveDash.java
@@ -0,0 +1,15 @@
+package com.example.vivian.buddysystem.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.vivian.buddysystem.R;
+
+public class NotActiveDash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_active_dash);
+    }
+}
